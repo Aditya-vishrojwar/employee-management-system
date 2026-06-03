10ba0dd371d18759b69cc02664f520744796606c
Aditya vishrojwar <vishrojwaraditya@gmail.com>
2026-06-03 14:03:23 +0530

    Initial Employee Management System

---
+++ src/main/java/com/aditya/employeeapp/EmployeeAdityaApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.employeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAdityaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAdityaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aditya/employeeapp/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.aditya.employeeapp.controller;
+
+import com.aditya.employeeapp.entity.Employee;
+import com.aditya.employeeapp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    @PostMapping(value = "/saveEmp")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping(value = "/getAllEmp")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
