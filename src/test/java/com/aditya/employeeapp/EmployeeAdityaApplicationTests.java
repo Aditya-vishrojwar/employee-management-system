ae312a8edb1ec662710a89dad1ed6b23943e5ebc
Aditya vishrojwar <vishrojwaraditya@gmail.com>
2026-06-03 14:46:47 +0530

    Added GitHub Actions CI

--- src/test/java/com/aditya/employeeapp/EmployeeAdityaApplicationTests.java
+++ src/test/java/com/aditya/employeeapp/EmployeeAdityaApplicationTests.java
@@ -1,13 +1,13 @@
-package com.aditya.employeeapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmployeeAdityaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.aditya.employeeapp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EmployeeAdityaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
